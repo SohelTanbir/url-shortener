c966e8a3806c1ed0868a38e5b7e87ed2c719bbd0
swesadiqul <mdsadiqulislam390@gmail.com>
2023-11-09 01:50:24 +0600

    create server

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const connectDatabase = require('./database/databaseConnecion');
+
+const app  = express();
+app.use(cors());
+app.use(express.json());
+// Database connection
+connectDatabase();
+
+const urlRoute = require('./routes/urlRoute'); 
+
+// routes
+app.get("/", (req, res) => {
+    res.send("Welcome to URL Shortener App!");
+})
+
+app.use("/api/v1", urlRoute);
+
+
+// default Error Handler
+const errorHandler  = (err, req, res, next)=>{
+    if(req.headersSent){
+         return next(err.message);
+    }
+    res.status(500).json({
+        success:false,
+        message:err
+    })
+ }
+
+ app.use(errorHandler);
+
+// listen on port 5000
+app.listen(5000, ()=> {
+    console.log("listening on port 5000");
+})
+
+

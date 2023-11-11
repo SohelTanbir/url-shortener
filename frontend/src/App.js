367b88d3f20a5cf2fa5f284f39c76ca8abcd6b4f
SohelTanbir <soheltanbir19@gmail.com>
2023-11-11 12:18:05 +0600

    get user input url and slgu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,36 @@ import { useState } from 'react';
 
 
 function App() {
-  const [inputUrl, setInputUrl] = useState("");
-  const [isError, setIsError ] =useState("sssss");
+  const [inputUrl, setInputUrl] = useState({
+    url:'',
+    slug:''
+  });
+  const [shortenUrl, setShortenUrl ] = useState("");
+  const [isError, setIsError ] =useState("");
 
   // get user input url
   const handleChange = (e) => {
-    setInputUrl(e.target.value);
+    const newUrl = {...inputUrl};
+    newUrl[e.target.name] = e.target.value;
+    setInputUrl(newUrl);
   }
 
 // handle submit the url
 const handleSubmit = (e) => {
   e.preventDefault();
-  console.log(inputUrl);
-  setInputUrl("");  
+
+  if(!inputUrl.url){
+    alert("Please Enter Your URL");
+    return;
+  }
+  console.log(inputUrl)
+  setInputUrl({
+    url:'',
+    slug:'',
+  })
 }
+
+
 
 
 
@@ -28,11 +44,12 @@ const handleSubmit = (e) => {
           <p className="short-description">You can short your long url</p>
           <form onSubmit={handleSubmit}>
             <label>Long URL</label>
-            <input className={`${isError} ? 'error' :''`} onChange={handleChange} type="text" placeholder="htts://example.con" value={inputUrl} />
+            <input className={`${isError ? 'error':''}`} onChange={handleChange} type="text" name='url' placeholder="htts://example.con" value={inputUrl.url} />
             <label>Slug (Optional)</label>
-            <input  onChange={handleChange} type="text" placeholder="htts://example.con" value={inputUrl} />
+            <input  onChange={handleChange} type="text" name='slug' placeholder="slug" value={inputUrl.slug} />
             <button className='generate-url-btn'>Generate URL</button>
           </form>
+         { shortenUrl&&
           <div className="output-url">
             <label>Shorten URL</label>
             <div className="shorten-url">
@@ -40,6 +57,7 @@ const handleSubmit = (e) => {
               <button className='copy-url-btn'>Copy</button>
             </div>    
           </div>
+          }
         </div>
     </div>
   );
